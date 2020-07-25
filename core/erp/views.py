73e5e6b9547a89e50a4420d6bab4c5a73a8604b4
Rafless <rfg844@gmail.com>
2020-07-25 13:26:01 -0500

    Creacion de Modelos para Trabajar

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def myfirstview(request):
+	data = {
+		'nombre': 'Rafael'
+
+	}
+	return render(request, 'index.html', data)
